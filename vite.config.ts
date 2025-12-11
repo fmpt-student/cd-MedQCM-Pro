c9805e6c4be48552cbc4536aa94736e5d5f4cf43
linahousni8-cmd <linahousni8@gmail.com>
2025-12-11 19:37:12 +0100

    modification uniquement de vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Assure que les chemins sont relatifs pour GitHub Pages
+  base: '/cd-MedQCM-Pro/', // Assure que les chemins sont relatifs pour GitHub Pages
   build: {
     outDir: 'dist',
   }
